be1f9f4f50cb606c39c81a5779df220720336b41
Bavo27 <bvandenh@macalester.edu>
2025-11-06 17:26:17 -0500

    buttons started

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Bavo's secret special website</header>
+      <button>Comp 225</button>
+      <button>Political Economy</button>
+      <button>Comp 221</button>
+      <button>Chinese</button>
+    </div>
+  );
+}
+
+export default App;
