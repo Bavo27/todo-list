c323fdcee79c3d0233a6844fa027eedadacffa49
Bavo27 <bvandenh@macalester.edu>
2025-11-21 23:45:55 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import class1 from './components/class1';
+import class1 from './components/Class1';
 
 function App() {
   return (
     <div className="App">
       <header>Bavo's classes</header>
-      <button>Comp 225</button>
+      <button onClick={change}>Comp 225</button>
       <button>Political Economy</button>
       <button>Comp 221</button>
       <button>Chinese</button>
     </div>
   );
 }
+function change() {
+  <class1 />
+  }
 
 export default App;
--- src/components/class1.js
+++ src/components/class1.js
@@ -1,2 +1,10 @@
 import React from "react";
 
+export default function class1() {
+  return (
+    <div>
+      <h2>Class 1 Component</h2>
+      <p>This is a placeholder for class1 component content.</p>
+    </div>
+  );
+}
